e42571347b725ca149cec74fb0a6f2840d3ee021
setfocusbrasil <114240224+setfocus2022@users.noreply.github.com>
2024-03-14 16:41:47 -0300

    logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -40,7 +40,7 @@ export const Header = () => {
               <Image
                 src="/icons/logo_transparente.png"
                 alt="SetFocus Brasil"
-                width="120"
+                width="150"
                 height="102"
               />
             </a>
