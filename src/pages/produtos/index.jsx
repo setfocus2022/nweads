f22fa27c2bef227bdd2ec87772393ac38b011569
setfocusbrasil <114240224+setfocus2022@users.noreply.github.com>
2024-03-15 16:43:56 -0300

    produtos

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -17,32 +17,32 @@ export default function Tratamentos() {
   return (
     <>
       <Head>
-        <title>Produtos | Sites prontos com dominio e marketing SEO integrado com o Google</title>
+        <title>Viva Imóveis Avalie | Avaliação de Imóveis - Goiânia,Goianira,Senador Canedo,parecida de Goiania,Trindade</title>
         <meta
           name="description"
-          content="As melhores marcas. Produtos certificados e qualificados"
+          content="Viva Imóveis Avalie | Avaliação de Imóveis Goiânia,Goianira,Senador Canedo,parecida de Goiania,Trindade"
         />
         <meta
           name="keywords"
-          content="marketing digital, site, contrução de site, página institucional"
+          content="Viva Imóveis Avalie | Avaliação de Imóveis Goiânia,Goianira,Senador Canedo,parecida de Goiania,Trindade"
         />
-        <meta name="author" content=" Marketing Digital - SetFocus Brasil " />
+        <meta name="author" content=" Viva Imóveis Avalie | Avaliação de Imóveis Goiânia,Goianira,Senador Canedo,parecida de Goiania,Trindade " />
       </Head>
 
       <div>
-        <HeaderPage subtitle="" title="  Sites sob medida para seu negócio" />
+        <HeaderPage subtitle="" title="  Viva Imóveis Avalie | Avaliação de Imóveis Goiânia,Goianira,Senador Canedo,parecida de Goiania,Trindade" />
       </div>
       <main className={`${styles.treatments} mainContainer`}>
 
         <div className={styles.treatmentsItem} data-aos="fade-up">
           <MyImage
             src="/images/pizzaria.jpg"
-            alt="Pizzaria e Lanchonetes - Delivery em Geral"
+            alt="Opinião Profissional de Valor"
             width="350"
             height="279"
             left
           />
-          <h3> Delivery em Geral </h3>
+          <h3> Opinião Profissional de Valor </h3>
           <div className={styles.description}>
             <p>
             Site disk entrega para Pizzaria com botão que chama o Whatsapp direto para o pedido.
@@ -67,11 +67,11 @@ export default function Tratamentos() {
         <div className={styles.treatmentsItem} data-aos="fade-up">
           <MyImage
             src="/images/prod_2.png"
-            alt="Materiais Elétricos"
+            alt="Parecer Técnico de Avaliação Mercadológica"
             width="350"
             height="279"
           />
-          <h3>Materiais Elétricos</h3>
+          <h3>Parecer Técnico de Avaliação Mercadológica </h3>
           <div className={styles.description}>
           <p>
             Site para Loja de Material Elétrico com foco no fortalecmento da marca e aumento da area de atuação de vendas.
@@ -95,11 +95,11 @@ export default function Tratamentos() {
         <div className={styles.treatmentsItem} data-aos="fade-up">
           <MyImage
             src="/images/prod_3.png"
-            alt="Materiais Elétricos"
+            alt="Parecer Técnico de Avaliação Mercadologica Judicial"
             width="350"
             height="279"
           />
-          <h3> Clínica Odontológica </h3>
+          <h3> Parecer Técnico de Avaliação Mercadologica Judicial</h3>
           <div className={styles.description}>
           <p>
           Site institucional para Clínica Odontológica com botão que chama o Whatsapp direto para a atendente.
@@ -120,60 +120,7 @@ export default function Tratamentos() {
         </div>
 
 
-          <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_4.png"
-            alt="Materiais Elétricos"
-            width="350"
-            height="279"
-          />
-          <h3>Eletro-Refrigeração</h3>
-          <div className={styles.description}>
-          <p>
-            Site para empresas que trabalham com venda e manutenção de equipamentos de refrigeração.
-        
-          <p>Desenvolvido em ReactJs de alto desempenho, rapidez e SEO otimizado para o Google, além de estar integrado com o Google Analytcs e Ads para um melhor acompanhamento do desempenho. </p>
-           <p>Valor já inclui o registro do domínio por 01 ano. Brinde hospedagem grátis por 01 ano. 
-           Não perca tempo e tenha uma site de alta tecnologia já com o seu dominio .com.br e alavanque suas Vendas.</p>
-           <p>Veja modelo  :</p>   
-           <Link href="https://eder-refrigeracao.vercel.app/">
-                  <a target="_blank">
-                  Eder Refrigeração - Site Demonstrativo
-                  </a>
-                </Link>
-           
-            </p>
-           
-          </div>
-        </div>
 
-
-         <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_5.png"
-            alt="Materiais Elétricos"
-            width="350"
-            height="279"
-          />
-          <h3>Depósito de Gás</h3>
-          <div className={styles.description}>
-          <p>
-            Site para depósito de gás com foco no fortalecmento da marca e aumento da area de atuação de vendas.
-        
-          <p>Desenvolvido em ReactJs de alto desempenho, rapidez e SEO otimizado para o Google, além de estar integrado com o Google Analytcs e Ads para um melhor acompanhamento do desempenho. </p>
-           <p>Valor já inclui o registro do domínio por 01 ano. Brinde hospedagem grátis por 01 ano. 
-           Não perca tempo e tenha uma site de alta tecnologia já com o seu dominio .com.br e alavanque suas Vendas.</p>
-           <p>Veja modelo  :</p>   
-           <Link href="https://paivagas.vercel.app/">
-                  <a target="_blank">
-                  Paiva Gás Lençóis Paulista - Site demonstrativo
-                  </a>
-                </Link>
-           
-            </p>
-           
-          </div>
-        </div>
 
 
   
