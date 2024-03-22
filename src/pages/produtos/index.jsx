59d4cb46a11ccd1b29ff1eaf7e651b7f0e5e5af9
setfocusbrasil <114240224+setfocus2022@users.noreply.github.com>
2024-03-22 10:40:29 -0300

    jhkbjk

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -58,7 +58,7 @@ export default function Tratamentos() {
             left
           />
           <h3> Opinião Profissional de Valor </h3>
-         <font face="Arial" color="#002d4b" > Indicado para negociação entre particulares </font>
+         <br><font face="Arial" color="#002d4b" > Indicado para negociação entre particulares </font></br>
    
           <div className={styles.description}>
            
