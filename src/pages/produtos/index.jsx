9a38b5d02fd8e713f3ddaf4d1743f203c459e521
setfocusbrasil <114240224+setfocus2022@users.noreply.github.com>
2024-03-22 10:22:01 -0300

    end

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -101,7 +101,7 @@ Se esse é o serviço que resolve o seu problema, clique no botão e me chame no
             height="279"
           />
           <h3>Parecer Técnico de Avaliação Mercadológica </h3>
-          <p><font face="Arial" color="blue" >Este serviço é ideal quando você precisa oferecer um imóvel como garantia para empresas, pessoas físicas e até algumas instituições financeiras, como consórcios ou operações de crédito.</font>
+          <p><font face="Arial" color="blue" >Este serviço é ideal quando você precisa oferecer um imóvel como garantia para empresas, pessoas físicas e até algumas instituições financeiras, como consórcios ou operações de crédito.</font></p>
           <div className={styles.description}>
           <p>
         
