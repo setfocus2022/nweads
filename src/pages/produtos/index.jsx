20260634b2915b224c68d5f1cfbe72be7513b759
setfocusbrasil <114240224+setfocus2022@users.noreply.github.com>
2024-03-22 12:27:24 -0300

    iiiiiii

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -99,7 +99,7 @@ Se esse é o serviço que resolve o seu problema, clique no botão e me chame no
             height="279"
           />
           <h3>Parecer Técnico de Avaliação Mercadológica </h3>
-         <strong><font face="Arial Black" color="#002d4b" >Este serviço é ideal quando você precisa oferecer um imóvel como garantia para empresas, pessoas físicas e até algumas instituições financeiras, como consórcios ou operações de crédito.</font></strong> 
+         <strong><font face="Arial" color="#002d4b" > <i>Este serviço é ideal quando você precisa oferecer um imóvel como garantia para empresas, pessoas físicas e até algumas instituições financeiras, como consórcios ou operações de crédito.</i></font></strong> 
           <div className={styles.description}>
             <p>   </p>
           <p>
@@ -139,7 +139,7 @@ Se você precisa de um Parecer Técnico de Avaliação Mercadológica para garan
             height="279"
           />
           <h3> Parecer Técnico de Avaliação Mercadologica Judicial</h3>
-          <font face="Arial" color="#002d4b" >Indicado para todos tipos de processos judiciais, como separações, partilhas de herança, avaliação patrimonial e qualquer outro processo que envolva o poder judicial</font>
+          <font face="Arial" color="#002d4b" >  <i>Indicado para todos tipos de processos judiciais, como separações, partilhas de herança, avaliação patrimonial e qualquer outro processo que envolva o poder judicial </i></font>
           <div className={styles.description}>
           <p>   </p>
           <p>
