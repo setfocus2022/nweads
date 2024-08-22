2654aea8f219a13eed752f107e033bced9180e05
setfocusbrasil <114240224+setfocus2022@users.noreply.github.com>
2024-08-22 14:56:45 -0300

    save ur

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -55,6 +55,161 @@ export default function Home() {
                
       </Head>
 
+      <div>
+        <HeaderPage subtitle="Avaliação de Imóveis"    />
+       
+      </div>
+      <main className={`${styles.treatments} mainContainer`}>
+
+        <div className={styles.treatmentsItem} data-aos="fade-up">
+      {/*   <video width="320" height="240" poster="/images/avalie24.png" controls preload="none">
+      <source src="/images/OPV.mp4" type="video/mp4" />
+      <track
+        src="/path/to/captions.vtt"
+        kind="subtitles"
+        srcLang="en"
+        label="English"
+      />
+      Your browser does not support the video tag.
+    </video>   */} 
+
+    <MyImage
+            src="/images/week4.jpeg"
+            alt="Pizzaria e Lanchonetes - Delivery em Geral"
+            width="350"
+            height="279"
+            left
+          />
+
+          <h3> Palio Adventure Locker </h3>
+         <font face="Arial" color="blue" > <i>Valor à Vista R$ 35990</i> </font>
+   
+          <div className={styles.description}>
+           
+          <p>   </p>       
+            <p>
+          <p> - Ano 2003 </p>
+
+          <p> Modelo Adventure Locker</p> 
+
+          <p> Completa </p>
+           <p> </p>
+           <p></p>  
+      
+         
+
+           <Link href="">
+                  <a target="_blank">
+                  
+                  </a>
+                </Link>
+
+                   
+            </p>
+          </div>
+          <Button
+                onClick={agendarConsulta}
+                link="https://wa.me//556293068692?text= Olá, eu vim da sua página de ADS Auto e me interessei pelo veículo Palio Adventure Locker "
+              >
+                <img src="/icons/whatsIcon.svg" alt="whatsapp" />
+                Fale com o Vendedor
+              </Button>    
+        </div>
+
+
+        <div className={styles.treatmentsItem} data-aos="fade-up">
+        <video width="320" height="240" poster="/images/avalie25.png" controls preload="none">
+      <source src="/images/PTAM.mp4" type="video/mp4" />
+      <track
+        src="/path/to/captions.vtt"
+        kind="subtitles"
+        srcLang="en"
+        label="English"
+      />
+      Your browser does not support the video tag.
+    </video>
+          <h3>Parecer Técnico de Avaliação Mercadológica </h3>
+         <strong><font face="Arial" color="blue" > <i>Este serviço é ideal quando você precisa oferecer um imóvel como garantia para empresas, pessoas físicas e até algumas instituições financeiras, como consórcios ou operações de crédito.</i></font></strong> 
+          <div className={styles.description}>
+            <p>   </p>
+          <p>
+        
+        
+          <p>Nosso Parecer Técnico de Avaliação Mercadológica é indicado para uma variedade de situações, especialmente para negócios entre empresas e particulares, empresas com outras empresas ou até mesmo entre pessoas físicas que precisam de um assessoramento mais completo nos valores. Este serviço é ideal quando você precisa oferecer um imóvel como garantia para empresas, pessoas físicas ou instituições financeiras, como consórcios ou operações de crédito.<p></p>
+
+<p>Com nosso parecer técnico, você terá uma avaliação mercadológica detalhada e fundamentada, proporcionando segurança e confiança em suas transações. Este documento é aceito por diversas instituições e pode ser utilizado para respaldar valores em uma variedade de operações comerciais.</p>
+
+Se você precisa de um Parecer Técnico de Avaliação Mercadológica para garantir o sucesso de suas transações comerciais, clique no botão abaixo e entre em contato conosco. Estamos prontos para oferecer o suporte necessário para suas necessidades empresariais.
+</p>
+           <p></p>
+           <p></p>   
+           <Link href="">
+                  <a target="_blank">
+                  
+                  </a>
+                </Link>
+              
+            </p>
+           
+          </div>
+          <Button
+                onClick={agendarConsulta}
+                link="https://wa.me//556293068692?text=Olá, eu vim da sua página de avaliação de imóveis e me interessei por PARECER TÉCNICO DE AVALIAÇÃO JUDICIAL como funciona a contratação da avaliação? "
+              >
+                <img src="/icons/whatsIcon.svg" alt="whatsapp" />
+                Clique aqui
+              </Button>  
+        </div>
+
+
+        <div className={styles.treatmentsItem} data-aos="fade-up">
+        <video width="320" height="240"  poster="/images/avalie26.png" controls preload="none">
+      <source src="/images/PTAMJ.mp4" type="video/mp4" />
+      <track
+        src="/path/to/captions.vtt"
+        kind="subtitles"
+        srcLang="en"
+        label="English"
+      />
+      Your browser does not support the video tag.
+    </video>
+          <h3> Parecer Técnico de Avaliação Mercadológica Judicial</h3>
+          <font face="Arial" color="blue" >  <i>Indicado para todos tipos de processos judiciais, como separações, partilhas de herança, avaliação patrimonial e qualquer outro processo que envolva o poder judicial </i></font>
+          <div className={styles.description}>
+          <p>   </p>
+          <p>
+
+        
+        
+          <p>Nosso Parecer Técnico de Avaliação Mercadológica Judicial segue os mais rigorosos padrões de qualidade, semelhante ao PTAM Comercial, conforme normas NBR que regem os padrões técnicos. No entanto, este serviço é especialmente elaborado com linguagem jurídica, tornando-o ideal para processos judiciais.</p>
+
+<p>Com nosso parecer técnico judicial, você terá uma avaliação robusta e fundamentada, aceita em todos os tipos de processos judiciais. Este serviço é essencial para casos comuns, como separações, partilhas de herança, avaliação patrimonial e qualquer outro processo que envolva o poder judicial. 
+  Seja para contestar laudos emitidos por outros profissionais, seja para servir como referência para partes envolvidas, demandadas, ou mesmo para auxiliar o trabalho do magistrado, nosso serviço oferece a segurança e confiabilidade necessárias em qualquer contexto jurídico.</p>
+
+<p>Se você precisa de um Parecer Técnico de Avaliação Mercadológica Judicial para fortalecer sua posição em um processo judicial, clique no botão abaixo e entre em contato conosco. Estamos prontos para oferecer o suporte técnico e jurídico necessário para o sucesso de sua causa.
+É também utilizado para contestar um laudo já emitido por outro profissional em um processo judicial, desde que o perito entenda que haja fundamento para tal contestação.
+</p>
+           <p></p>
+           <p></p>   
+           <Link href="">
+                  <a target="_blank">
+                  
+                  </a>
+                </Link>
+                
+            </p>
+           
+          </div>
+          <Button
+                onClick={agendarConsulta}
+                link="https://wa.me//556293068692?text=Olá, eu vim da sua página de avaliação de imóveis e me interessei por PARECER TÉCNICO DE AVALIAÇÃO JUDICIAL como funciona a contratação da avaliação? "
+              >
+                <img src="/icons/whatsIcon.svg" alt="whatsapp" />
+                Clique aqui
+              </Button>  
+        </div>
+
+
       <div className={styles.schedule}>
         <p>
           Aberto: Seg - Sex: 08:00 - 18:00 | Av. Principal, 999 - Lençóis
