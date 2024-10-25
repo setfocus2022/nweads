6e1caf077d060b8b822cc0b2fc9d9b053a8a50db
setfocusbrasil <114240224+setfocus2022@users.noreply.github.com>
2024-10-25 14:54:36 -0300

    analytics

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -39,7 +39,15 @@ export default function Home() {
      crossorigin="anonymous"></script>
       
       
+      <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-61KK36TT9J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-61KK36TT9J');
+</script>
 
 
        <title>Home | Ads Auto - Marketing para Venda de Carros </title> 
